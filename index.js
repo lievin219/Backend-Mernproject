47c5b9450cc3a588e66bf1d9058358bf25d51cc6
lievin219 <gakizalievin219@gmail.com>
2024-10-08 09:34:49 +0200

    authentication modified

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin:["http://localhost:3000","http://localhost:5173"],
+  origin:["http://localhost:3000","http://localhost:5173","https://lievin219.github.io"],
    
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
